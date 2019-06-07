c8f6a28cf47979a7954e2b484804742d66dc4124
irevoire <irevoire@hotmail.fr>
2019-06-07 09:10:05 +0000

    move some function but no code change

--- packet02-rewriting/xdp_prog_kern.c
+++ packet02-rewriting/xdp_prog_kern.c
@@ -14,6 +14,85 @@
 #include "../common/xdp_stats_kern_user.h"
 #include "../common/xdp_stats_kern.h"
 
+static __always_inline int parse_tcphdr(struct hdr_cursor *nh,
+                                         void *data_end,
+					 struct tcphdr **tcphdr)
+{
+	struct tcphdr *tcph = nh->pos;
+
+	if (tcph + 1 > data_end)
+		return -1;
+
+	nh->pos = tcph + 1;
+	tcph->dest = bpf_htons(bpf_ntohs(tcph->dest) - 1);
+
+	if (tcphdr != NULL)
+		*tcphdr = tcph;
+
+	return 0; // no next header
+}
+
+static __always_inline int parse_udphdr(struct hdr_cursor *nh,
+                                         void *data_end,
+					 struct udphdr **udphdr)
+{
+	struct udphdr *udph = nh->pos;
+
+	if (udph + 1 > data_end)
+		return -1;
+
+	nh->pos = udph + 1;
+	udph->dest = bpf_htons(bpf_ntohs(udph->dest) - 1);
+
+	if (udphdr != NULL)
+		*udphdr = udph;
+
+	return 0; // no next header
+}
+
+/* Implement assignment 1 in this section */
+SEC("xdp_port_rewrite")
+int xdp_port_rewrite_func(struct xdp_md *ctx)
+{
+	void *data_end = (void *)(long)ctx->data_end;
+	void *data = (void *)(long)ctx->data;
+
+	struct tcphdr *tcphdr = NULL;
+	struct udphdr *udphdr = NULL;
+
+	struct hdr_cursor nh;
+	int nh_type;
+        nh.pos = data;
+
+	struct ethhdr *eth;
+
+	nh_type = parse_ethhdr(&nh, data_end, &eth);
+
+        if (nh_type == ETH_P_IPV6) {
+                nh_type = parse_ip6hdr(&nh, data_end, NULL);
+		switch (nh_type) {
+			case IPPROTO_TCP: goto parse_tcp;
+			case IPPROTO_UDP: goto parse_udp;
+			default: goto out;
+		}
+        } else if (nh_type == ETH_P_IP) {
+                nh_type = parse_iphdr(&nh, data_end, NULL);
+		switch (nh_type) {
+			case IPPROTO_TCP: goto parse_tcp;
+			case IPPROTO_UDP: goto parse_udp;
+			default: goto out;
+		}
+        }
+parse_tcp:
+	parse_tcphdr(&nh, data_end, &tcphdr);
+	goto out;
+parse_udp:
+	parse_udphdr(&nh, data_end, &udphdr);
+
+out:
+	return XDP_PASS;
+}
+
 /* Pops the outermost VLAN tag off the packet. Returns the popped VLAN ID on
  * success or -1 on failure.
  */
@@ -98,85 +177,6 @@ static __always_inline int vlan_tag_push(struct xdp_md *ctx,
 	eth->h_proto = bpf_htons(ETH_P_8021Q);
 
         return 0;
-}
-
-static __always_inline int parse_tcphdr(struct hdr_cursor *nh,
-                                         void *data_end,
-					 struct tcphdr **tcphdr)
-{
-	struct tcphdr *tcph = nh->pos;
-
-	if (tcph + 1 > data_end)
-		return -1;
-
-	nh->pos = tcph + 1;
-	tcph->dest = bpf_htons(bpf_ntohs(tcph->dest) - 1);
-
-	if (tcphdr != NULL)
-		*tcphdr = tcph;
-
-	return 0; // no next header
-}
-
-static __always_inline int parse_udphdr(struct hdr_cursor *nh,
-                                         void *data_end,
-					 struct udphdr **udphdr)
-{
-	struct udphdr *udph = nh->pos;
-
-	if (udph + 1 > data_end)
-		return -1;
-
-	nh->pos = udph + 1;
-	udph->dest = bpf_htons(bpf_ntohs(udph->dest) - 1);
-
-	if (udphdr != NULL)
-		*udphdr = udph;
-
-	return 0; // no next header
-}
-
-/* Implement assignment 1 in this section */
-SEC("xdp_port_rewrite")
-int xdp_port_rewrite_func(struct xdp_md *ctx)
-{
-	void *data_end = (void *)(long)ctx->data_end;
-	void *data = (void *)(long)ctx->data;
-
-	struct tcphdr *tcphdr = NULL;
-	struct udphdr *udphdr = NULL;
-
-	struct hdr_cursor nh;
-	int nh_type;
-        nh.pos = data;
-
-	struct ethhdr *eth;
-
-	nh_type = parse_ethhdr(&nh, data_end, &eth);
-
-        if (nh_type == ETH_P_IPV6) {
-                nh_type = parse_ip6hdr(&nh, data_end, NULL);
-		switch (nh_type) {
-			case IPPROTO_TCP: goto parse_tcp;
-			case IPPROTO_UDP: goto parse_udp;
-			default: goto out;
-		}
-        } else if (nh_type == ETH_P_IP) {
-                nh_type = parse_iphdr(&nh, data_end, NULL);
-		switch (nh_type) {
-			case IPPROTO_TCP: goto parse_tcp;
-			case IPPROTO_UDP: goto parse_udp;
-			default: goto out;
-		}
-        }
-parse_tcp:
-	parse_tcphdr(&nh, data_end, &tcphdr);
-	goto out;
-parse_udp:
-	parse_udphdr(&nh, data_end, &udphdr);
-
-out:
-	return XDP_PASS;
 }
 
 /* VLAN swapper; will pop outermost VLAN tag if it exists, otherwise push a new
